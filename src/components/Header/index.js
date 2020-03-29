8a938a6fae36f32c20f8e3e5d56201cb0ae61989
Roberto <roberto_junior_sousa@hotmail.com>
2020-03-29 00:41:09 -0300

    adicionado componente Header para alteração de theme

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
+
+import { Container } from './styles';
+
+export default function Header({ toggleTheme }) {
+  const { colors, title, logo } = useContext(ThemeContext);
+
+  return (
+    <Container>
+      <img src={logo} alt="Be The Hero" width={90} />
+      <Switch
+        onChange={toggleTheme}
+        checked={title === 'dark'}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={colors.secundary}
+        onColor={colors.text}
+        offHandleColor={colors.secundary}
+        onHandleColor={colors.text}
+      />
+    </Container>
+  );
+}
+
+Header.propTypes = {
+  toggleTheme: PropTypes.func.isRequired,
+};
