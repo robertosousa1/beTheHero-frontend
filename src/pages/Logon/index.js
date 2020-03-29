f5ad881636abfde8b8b719d26e879a9aa385ec05
Roberto <roberto_junior_sousa@hotmail.com>
2020-03-29 00:39:29 -0300

    alterado para styled components e theme color

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,32 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+import { ThemeContext } from 'styled-components';
 
-import './styles.css';
+import { Container, SectionForm, Button, Form } from './styles';
 
-import heroesImg from '../../assets/heroes.png';
-import logoImg from '../../assets/logo.svg';
+import HeroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+  const { logo } = useContext(ThemeContext);
+
   return (
-    <div className="logon-container">
-      <section className="form">
-        <img src={logoImg} alt="Be The Hero" />
-        <form>
+    <Container>
+      <SectionForm>
+        <img src={logo} alt="Be The Hero" />
+
+        <Form onSubmit={() => {}}>
           <h1>Faça seu logon</h1>
-
           <input placeholder="Sua ID" />
-          <button className="button" type="submit">
-            Entrar
-          </button>
+          <Button type="submit">Entrar</Button>
 
-          <Link className="back-link" to="/register">
-            <FiLogIn size={16} color="#e02041" />
+          <Link to="/register">
+            <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
           </Link>
-        </form>
-      </section>
-      <img src={heroesImg} alt="heroes" />
-    </div>
+        </Form>
+      </SectionForm>
+
+      <img src={HeroesImg} alt="Heroes" />
+    </Container>
   );
 }
