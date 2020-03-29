ecc8ba9f181ad6c81284531ba03b39695df70a16
Roberto <roberto_junior_sousa@hotmail.com>
2020-03-29 00:44:44 -0300

    alterado para styled components e theme color

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,42 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { ThemeContext } from 'styled-components';
 
-import './styles.css';
+import {
+  Container,
+  Content,
+  Section,
+  InputGroup,
+  Button,
+  Form,
+} from './styles';
 
-import heroesImg from '../../assets/heroes.png';
-import logoImg from '../../assets/logo.svg';
+export default function Register() {
+  const { logo } = useContext(ThemeContext);
 
-export default function Logon() {
   return (
-    <div className="register-container">
-      <div className="content">
-        <section className="form">
-          <img src={logoImg} alt="Be The Hero" />
+    <Container>
+      <Content>
+        <Section>
+          <img src={logo} alt="Be The Hero" />
 
           <h1>Cadastro</h1>
           <p>
             Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
-            os casos da sua ONG.
+            os casos de sua ONG.
           </p>
-          <Link className="back-link" to="/">
-            <FiArrowLeft size={16} color="#e02041" />
-            Voltar
+
+          <Link to="/">
+            <FiArrowLeft size={16} color="#E02041" />
+            Já tenho cadastro
           </Link>
-        </section>
-        <form>
+        </Section>
+        <Form onSubmit={() => {}}>
           <input placeholder="Nome da ONG" />
           <input type="email" placeholder="E-mail" />
           <input placeholder="WhatsApp" />
-          <div className="input-group">
+          <InputGroup>
             <input placeholder="Cidade" />
             <input placeholder="UF" style={{ width: 80 }} />
-          </div>
-          <button className="button" type="submit">
-            Cadastrar
-          </button>
-        </form>
-      </div>
-    </div>
+          </InputGroup>
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+      </Content>
+    </Container>
   );
 }
