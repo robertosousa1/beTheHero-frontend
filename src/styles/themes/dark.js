d18fce3caab19e3d440c463e677a7905adf15dbf
Roberto <roberto_junior_sousa@hotmail.com>
2020-03-29 00:38:21 -0300

    adicionado theme dark e light

---
+++ src/styles/themes/dark.js
@@ -0,0 +1,16 @@
+import logoDark from '../../assets/logo-dark.svg';
+
+export default {
+  title: 'dark',
+  logo: `${logoDark}`,
+
+  colors: {
+    primary: '#0D0D0D',
+    secundary: '#E02041',
+
+    background: '#0C090D',
+    text: '#F5F5F5',
+    content: '#202020',
+    input: '#191919',
+  },
+};
---
+++ src/styles/themes/light.js
@@ -0,0 +1,16 @@
+import logoLight from '../../assets/logo-light.svg';
+
+export default {
+  title: 'light',
+  logo: `${logoLight}`,
+
+  colors: {
+    primary: '#0C090D',
+    secundary: '#E02041',
+
+    background: '#f0f0f5',
+    text: '#0C090D',
+    content: '#F5F5F5',
+    input: '#fff',
+  },
+};
