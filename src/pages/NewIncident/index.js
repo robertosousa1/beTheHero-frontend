abdaa4932be9337117acad7084791fe2fac5fd0c
Roberto <roberto_junior_sousa@hotmail.com>
2020-03-29 02:00:44 -0300

    estrutura e estilização page NewIncident

---
+++ src/pages/NewIncident/index.js
@@ -0,0 +1,81 @@
+import React, { useState, useContext } from 'react';
+import { ThemeContext } from 'styled-components';
+
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+// import api from '../../services/api'
+
+import { Container, Content, Section, Button, Form } from './styles';
+
+export default function NewIncident() {
+  // const [title, setTitle] = useState('')
+  // const [description, setDescription] = useState('')
+  // const [value, setValue] = useState('')
+
+  // const history = useHistory()
+
+  // const ongId = localStorage.getItem('ongId')
+
+  async function handleNewIncident(e) {
+    // e.preventDefault()
+    // const data = {
+    //   title,
+    //   description,
+    //   value,
+    // }
+    // try {
+    //   await api.post('incidents', data, {
+    //     headers: {
+    //       Authorization: ongId,
+    //     },
+    //   })
+    //   history.push('/profile')
+    // } catch (err) {
+    //   alert('Erro ao cadastrar caso, tente novamente.')
+    // }
+  }
+
+  const { logo } = useContext(ThemeContext);
+
+  return (
+    <Container>
+      <Content>
+        <Section>
+          <img src={logo} alt="Be The Hero" />
+
+          <h1>Cadastrar novo caso</h1>
+          <p>
+            Descreva o caso detalhadamente para encontrar um herói para resolver
+            isso.
+          </p>
+
+          <Link to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para home
+          </Link>
+        </Section>
+        <Form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Título do caso"
+            // value={title}
+            // onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            // value={description}
+            // onChange={(e) => setDescription(e.target.value)}
+          />
+
+          <input
+            placeholder="Valor em reais"
+            // value={value}
+            // onChange={(e) => setValue(e.target.value)}
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+}
